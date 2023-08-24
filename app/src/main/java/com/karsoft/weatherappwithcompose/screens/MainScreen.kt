d3393ce9d33595e5ecf0081933c0e20dc03d8539
= <=>
2023-08-24 10:31:22 +0500

    added WeatherModel

--- app/src/main/java/com/karsoft/weatherappwithcompose/screens/MainScreen.kt
+++ app/src/main/java/com/karsoft/weatherappwithcompose/screens/MainScreen.kt
@@ -1,9 +1,12 @@
 package com.karsoft.weatherappwithcompose.screens
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -14,7 +17,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.pagerTabIndicatorOffset
+import com.google.accompanist.pager.rememberPagerState
 import com.karsoft.weatherappwithcompose.R
+import com.karsoft.weatherappwithcompose.data.WeatherModel
+import kotlinx.coroutines.launch
 
 @Preview(showBackground = true)
 @Composable
@@ -103,25 +112,84 @@ fun MainCard() {
     }
 }
 
+@OptIn(ExperimentalPagerApi::class)
 @Composable
 fun TabLayout() {
     val tabList = listOf("HOURS", "DAYS")
-    Column(modifier = Modifier.padding(5.dp).clip(RoundedCornerShape(5.dp))) {
+    val pagerState = rememberPagerState()
+    val coroutineScope = rememberCoroutineScope()
+
+    Column(modifier = Modifier
+        .padding(5.dp)
+        .clip(RoundedCornerShape(5.dp))) {
         TabRow(
             selectedTabIndex = 0,
-            indicator = {},
-            backgroundColor = Color.Magenta
+            indicator = { pos ->
+                        TabRowDefaults.Indicator(
+                            Modifier.pagerTabIndicatorOffset(pagerState, pos)
+                        )
+            },
+            backgroundColor = Color.Magenta,
+            contentColor = Color.White
         ) {
             tabList.forEachIndexed { index, text ->
                 Tab(
                     selected = false,
                     onClick = {
-
+                              coroutineScope.launch {
+                                  pagerState.animateScrollToPage(index)
+                              }
                     },
                     text = {
-                        Text(text = text)
+                        Text(text = text, color = Color.White)
                     }
                 )
+            }
+        }
+        HorizontalPager(
+            count = tabList.size,
+        state = pagerState,
+        modifier = Modifier.weight(1.0f)) {
+            index ->
+            LazyColumn(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                itemsIndexed(
+                    listOf(
+                        WeatherModel(
+                            "Nukus",
+                            "10:22",
+                            "28ºC",
+                            "Sunny",
+                            "//cdn.weatherapi.com/weather/64x64/day/113.png",
+                            "36ºC",
+                            "9ºC",
+                            "11:00"
+                        ),
+                        WeatherModel(
+                            "Nukus",
+                            "10:22",
+                            "40ºC",
+                            "Sunny",
+                            "//cdn.weatherapi.com/weather/64x64/day/113.png",
+                            "45ºC",
+                            "9ºC",
+                            "19:00"
+                        ),
+                        WeatherModel(
+                            "Nukus",
+                            "10:22",
+                            "",
+                            "Sunny",
+                            "//cdn.weatherapi.com/weather/64x64/day/113.png",
+                            "36ºC",
+                            "9ºC",
+                            "24.08.2023"
+                        )
+                    )
+                ){
+                    _, item -> ListItem(item = item)
+                }
             }
         }
     }
---
+++ app/src/main/java/com/karsoft/weatherappwithcompose/screens/UI.kt
@@ -0,0 +1,54 @@
+package com.karsoft.weatherappwithcompose.screens
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.karsoft.weatherappwithcompose.data.WeatherModel
+
+@Composable
+fun ListItem(item: WeatherModel) {
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 3.dp),
+        backgroundColor = Color.Magenta,
+        elevation = 0.dp,
+        shape = RoundedCornerShape(5.dp),
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(
+                modifier = Modifier.padding(
+                        start = 8.dp, top = 5.dp, bottom = 5.dp
+                    )
+            ) {
+                Text(text = item.time)
+                Text(text = item.condition, color = Color.White)
+            }
+            Text(text = item.currentTemp.ifEmpty { "${item.maxTemp}/${item.minTemp}" },
+                color = Color.White,
+                style = TextStyle(fontSize = 25.sp))
+            AsyncImage(
+                model = "https:" + item.icon,
+                contentDescription = "im5",
+                modifier = Modifier
+                    .size(35.dp)
+                    .padding(end = 5.dp)
+            )
+        }
+    }
+}
