751714f7efc4fcd08d0900fa7c24ecf914345600
= <=>
2023-08-24 09:34:05 +0500

    added TabLayout

--- app/src/main/java/com/karsoft/weatherappwithcompose/MainActivity.kt
+++ app/src/main/java/com/karsoft/weatherappwithcompose/MainActivity.kt
@@ -1,17 +1,17 @@
 package com.karsoft.weatherappwithcompose
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import com.karsoft.weatherappwithcompose.screens.MainCard
+import com.karsoft.weatherappwithcompose.screens.TabLayout
 import com.karsoft.weatherappwithcompose.ui.theme.WeatherAppWithComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,24 +19,19 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WeatherAppWithComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Hello World")
+                Image(
+                    painter = painterResource(id = R.drawable.bg_weather),
+                    contentDescription = "im1",
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .alpha(0.7f),
+                    contentScale = ContentScale.FillBounds
+                )
+                Column {
+                    MainCard()
+                    TabLayout()
                 }
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-    Text(text = "Alignment")
-    Text(text = "Alignment")
-    Text(text = "Hello My Friend")
-    Text(text = "Hello My Friend")
-    Text(text = "Hello My Friend")
 }
---
+++ app/src/main/java/com/karsoft/weatherappwithcompose/screens/MainScreen.kt
@@ -0,0 +1,128 @@
+package com.karsoft.weatherappwithcompose.screens
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.karsoft.weatherappwithcompose.R
+
+@Preview(showBackground = true)
+@Composable
+fun MainCard() {
+    Column(
+        modifier = Modifier
+            .padding(5.dp)
+    ) {
+        Card(
+            modifier = Modifier.fillMaxWidth(),
+            backgroundColor = Color.Magenta,
+            elevation = 0.dp,
+            shape = RoundedCornerShape(10.dp)
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        modifier = Modifier.padding(top = 8.dp, start = 8.dp),
+                        text = "23 August 2023 17:20",
+                        style = TextStyle(fontSize = 15.sp),
+                        color = Color.White
+                    )
+                    AsyncImage(
+                        model = "https://cdn.weatherapi.com/weather/64x64/day/113.png",
+                        contentDescription = "im2",
+                        modifier = Modifier
+                            .size(35.dp)
+                            .padding(top = 3.dp, end = 8.dp)
+                    )
+                }
+                Text(
+                    text = "Nukus",
+                    style = TextStyle(fontSize = 24.sp),
+                    color = Color.White
+                )
+                Text(
+                    text = "23°",
+                    style = TextStyle(fontSize = 64.sp),
+                    color = Color.White
+                )
+                Text(
+                    text = "Sunny",
+                    style = TextStyle(fontSize = 16.sp),
+                    color = Color.White
+                )
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    IconButton(
+                        onClick = {
+
+                        }
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_search),
+                            contentDescription = "im3",
+                            tint = Color.White
+                        )
+                    }
+                    Text(
+                        text = "23°C/12ºC",
+                        style = TextStyle(fontSize = 16.sp),
+                        color = Color.White
+                    )
+                    IconButton(
+                        onClick = {
+
+                        }
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_sync),
+                            contentDescription = "im4",
+                            tint = Color.White
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TabLayout() {
+    val tabList = listOf("HOURS", "DAYS")
+    Column(modifier = Modifier.padding(5.dp).clip(RoundedCornerShape(5.dp))) {
+        TabRow(
+            selectedTabIndex = 0,
+            indicator = {},
+            backgroundColor = Color.Magenta
+        ) {
+            tabList.forEachIndexed { index, text ->
+                Tab(
+                    selected = false,
+                    onClick = {
+
+                    },
+                    text = {
+                        Text(text = text)
+                    }
+                )
+            }
+        }
+    }
+}
